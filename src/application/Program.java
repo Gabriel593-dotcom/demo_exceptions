3f376849b78843c272403626dbe0e927a45e7ee0
gabriel <g.dos.pinheiro1999@gmail.com>
2020-03-22 15:22:28 -0300

    Solução ruim

--- src/application/Program.java
+++ src/application/Program.java
@@ -25,10 +25,59 @@ public class Program {
 		if (!checkOut.after(checkIn)) {
 			System.out.println("A data de Check-out deve ser posterior a data de Check-in");
 		}
-
+ 
 		else {
 			reservation = new Reservation(roomNumber, checkIn, checkOut);
-			System.out.println("Reservation: " + reservation /* ou reservaion.toString() */);
+			System.out.println("Reservation: " + reservation);
+			// Sem necessidade de colocar o metodo correspondente a impressão, porque é
+			// identificado automaticamente.
+		}
+
+		System.out.println("------------------------------------------");
+		System.out.println("Enter data to update the reservation: ");
+		System.out.print("Check-in data (dd/MM/yyyy)");
+		checkIn = sdf.parse(sc.next());
+		System.out.print("Check-out data (dd/MM/yyyy)");
+		checkOut = sdf.parse(sc.next());
+		
+		String error = reservation.updateDates(checkIn, checkOut);
+		
+		if(error != null) {
+			
+			System.out.println("error in reservation: " + error);
+		}
+		else {
+			
+			System.out.println("Reservation: " + reservation);
+		}
+		sc.close();
+	}
+
+}
+
+
+
+// SOLUÇÃO MUITO RUIM
+
+/*
+ Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		Reservation reservation = new Reservation();
+
+		System.out.print("Room number: ");
+		int roomNumber = sc.nextInt();
+		System.out.print("Check-in data (dd/MM/yyyy)");
+		Date checkIn = sdf.parse(sc.next());
+		System.out.print("Check-out data (dd/MM/yyyy)");
+		Date checkOut = sdf.parse(sc.next());
+
+		if (!checkOut.after(checkIn)) {
+			System.out.println("A data de Check-out deve ser posterior a data de Check-in");
+		}
+ 
+		else {
+			reservation = new Reservation(roomNumber, checkIn, checkOut);
+			System.out.println("Reservation: " + reservation  ou reservaion.toString() );
 			// Sem necessidade de colocar o metodo correspondente a impressão, porque é
 			// identificado automaticamente.
 		}
@@ -56,7 +105,53 @@ public class Program {
 
 		}
 
-		sc.close();
-	}
+		sc.close();*/
 
-}
+
+
+
+// RUIM
+
+/*
+ Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		Reservation reservation = new Reservation();
+
+		System.out.print("Room number: ");
+		int roomNumber = sc.nextInt();
+		System.out.print("Check-in data (dd/MM/yyyy)");
+		Date checkIn = sdf.parse(sc.next());
+		System.out.print("Check-out data (dd/MM/yyyy)");
+		Date checkOut = sdf.parse(sc.next());
+
+		if (!checkOut.after(checkIn)) {
+			System.out.println("A data de Check-out deve ser posterior a data de Check-in");
+		}
+ 
+		else {
+			reservation = new Reservation(roomNumber, checkIn, checkOut);
+			System.out.println("Reservation: " + reservation);
+			// Sem necessidade de colocar o metodo correspondente a impressão, porque é
+			// identificado automaticamente.
+		}
+
+		System.out.println("------------------------------------------");
+		System.out.println("Enter data to update the reservation: ");
+		System.out.print("Check-in data (dd/MM/yyyy)");
+		checkIn = sdf.parse(sc.next());
+		System.out.print("Check-out data (dd/MM/yyyy)");
+		checkOut = sdf.parse(sc.next());
+		
+		String error = reservation.updateDates(checkIn, checkOut);
+		
+		if(error != null) {
+			
+			System.out.println("error in reservation: " + error);
+		}
+		else {
+			
+			System.out.println("Reservation: " + reservation);
+		}
+		sc.close();
+ * */
+ */
--- src/model/entitites/Reservation.java
+++ src/model/entitites/Reservation.java
@@ -48,17 +48,29 @@ public class Reservation {
 
 	}
 
-	public void updateDates(Date checkIn, Date checkOut) {
+	public String updateDates(Date checkIn, Date checkOut) {
+
+		Date now = new Date();
+		if (checkIn.before(now) || checkOut.before(now)) {
+
+			return "As datas de reserva para atualização devem ser futuras.";
+		}
+
+		if (!checkOut.after(checkIn)) {
+			return "A data de Check-out deve ser posterior a data de Check-in";
+		}
 
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
+		
+		return null; // irá retorna null se a operação não der nenhum erro;
 	}
 
 	@Override
 	public String toString() {
 
-		return "Room: " + roomNumber + ", checkin: " + sdf.format(this.checkIn) + ", check-out " + sdf.format(this.checkOut) + ", "
-				+ this.duration() + " nights";
+		return "Room: " + roomNumber + ", checkin: " + sdf.format(this.checkIn) + ", check-out "
+				+ sdf.format(this.checkOut) + ", " + this.duration() + " nights";
 
 	}
 }
